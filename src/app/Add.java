a10d2acece7bf4b655c8e026dc6f29866d9f17da
shreyasmalewar <shreyasmalewar@gmail.com>
2020-04-30 21:53:30 +0530

    Added projects

---
+++ src/app/Add.java
@@ -0,0 +1,10 @@
+package app;
+
+public class Add {
+    public static void main(String[] args) {
+        int num1 = 5, num2 = 7, sum;
+        sum = num1 + num2;
+        System.out.printf("Sum of numbers = ", +sum);
+
+    }
+}
