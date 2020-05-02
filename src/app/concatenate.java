98125a8be11534a1fe691e07ce798cf8404f1bc7
shreyasmalewar <shreyasmalewar@gmail.com>
2020-05-02 21:10:00 +0530

    concatenate and date added

---
+++ src/app/concatenate.java
@@ -0,0 +1,8 @@
+package app;
+
+public class concatenate {
+    public static void main(String[] args) {
+        String name = "Shreyas" + " Malewar";
+        System.out.println(name);
+    }
+}
---
+++ src/app/date.java
@@ -0,0 +1,12 @@
+package app;
+
+import java.util.Date;
+
+public class date {
+    public static void main(String[] args) {
+        byte age = 32;
+        Date today = new Date();
+        System.out.println(age);
+        System.out.println(today);
+    }
+}
