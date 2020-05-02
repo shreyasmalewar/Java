883a90253b1b8d8c1eefb1dc64d894d3c1dd2e94
shreyasmalewar <shreyasmalewar@gmail.com>
2020-05-02 20:55:47 +0530

    Concatenation in strings implemented

--- learn-java/src/app/hello.java
+++ learn-java/src/app/hello.java
@@ -1,8 +1,8 @@
 package app;
 
-public class hello {
+public class hello{
     public static void main(String[] args) {
-        System.out.println("Hello Java");
-        System.out.println("I'm Shreyas");
+        String message = "Shreyas " + "Malewar";
+        System.out.println(message);
     }
 }
