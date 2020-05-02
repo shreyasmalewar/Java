32032cd46fc4e591e6d50ba2f857607885745e11
shreyasmalewar <shreyasmalewar@gmail.com>
2020-05-02 20:14:06 +0530

    Application of Date. Using java.util.date

--- learn-java/src/app/App.java
+++ learn-java/src/app/App.java
@@ -1,13 +1,11 @@
 package app;
 
+import java.util.Date;
+
 public class App{
     public static void main(String[] args) {
-        int num1 = 20;
-        int num2 = 13;
-        int sum;
-
-        sum = num1 + num2;
-
-        System.out.printf("Sum of %d and %d is %d", num1, num2, sum);
+        byte age = 30;
+        Date now = new Date();
+        System.out.println(now);
     }
 }
