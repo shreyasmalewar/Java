73adec62337842a95a3eec0ef218c9df1575fa49
shreyasmalewar <shreyasmalewar@gmail.com>
2020-05-02 21:18:14 +0530

    /newintegration test

--- src/app/concatenate.java
+++ src/app/concatenate.java
@@ -2,7 +2,7 @@ package app;
 
 public class concatenate {
     public static void main(String[] args) {
-        String name = "Shreyas" + " Malewar";
+        String name = "Shreyas" + " Sanjiv" + " Malewar";
         System.out.println(name);
     }
 }
