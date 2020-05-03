46f16e80727e2fbacc8ed3963970c1884d7beea5
shreyasmalewar <shreyasmalewar@gmail.com>
2020-05-03 20:12:10 +0530

    class name change concatenate -> strings

--- src/app/concatenate.java
+++
@@ -1,8 +0,0 @@
-package app;
-
-public class concatenate {
-    public static void main(String[] args) {
-        String name = "Shreyas" + " Sanjiv" + " Malewar";
-        System.out.println(name);
-    }
-}
