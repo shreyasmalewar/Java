314911d70374980a4f3b3f25a81def91030b5eeb
shreyasmalewar <shreyasmalewar@gmail.com>
2020-05-04 01:45:50 +0530

    Hate warnings. Commented code segment in userinput

--- src/basics/UserInput.java
+++ src/basics/UserInput.java
@@ -5,14 +5,14 @@ import java.util.Scanner;
 public class UserInput {
     public static void main(String[] args) {
         
-      Scanner scan = new Scanner(System.in);
+      /*Scanner scan = new Scanner(System.in);
 
       System.out.print("name :");
 
       String name = scan.nextLine().trim();
 
       System.out.println("Okay! You are " + name);
-      
+      */
     }
     
 }
