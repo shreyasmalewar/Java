d6c8e70f63bb8c3992ca25c4f9d9c07d26a38378
shreyasmalewar <shreyasmalewar@gmail.com>
2020-05-03 23:39:36 +0530

    1D arrays

---
+++ src/app/array.java
@@ -0,0 +1,11 @@
+package app;
+
+import java.util.Arrays;
+
+public class array {
+    public static void main(String[] args) {
+    int[] numbers = { 2, 3, 5, 1, 4};
+    Arrays.sort(numbers);
+    System.out.println(Arrays.toString(numbers));
+    }
+}
