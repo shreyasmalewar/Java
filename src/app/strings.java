9cdf3855094bf5db09d52aeebdc59760860f4d6d
shreyasmalewar <shreyasmalewar@gmail.com>
2020-05-03 20:14:33 +0530

    strings beautified

--- src/app/strings.java
+++ src/app/strings.java
@@ -2,15 +2,24 @@ package app;
 
 public class strings {
     public static void main(String[] args) {
-        String name = "Hey \"Shreyas\"";  //back slash is a special charecter
+
+        String name = "Hey Shreyas";  //back slash is a special charecter
+        
         System.out.println(name.endsWith("war"));
+        
         System.out.println(name.startsWith("shr"));
+        
         System.out.println(name.length());
+        
         System.out.println(name.indexOf("eya"));
-        //System.out.println(name.replace("Male", "Iran"));
+        
+        System.out.println(name.replace("s", "S"));
         System.out.println(name);
+        
         System.out.println(name.toLowerCase());
+        
         System.out.println(name.trim());
+        
         System.out.println(name);
     }
 }
