efcd3a6c9ecb622a4326a8c6f9b89d94dc55a102
shreyasmalewar <shreyasmalewar@gmail.com>
2020-05-04 00:56:04 +0530

    Documentation added and beautified

--- src/app/FormatNumber.java
+++ src/app/FormatNumber.java
@@ -4,12 +4,14 @@ import java.text.NumberFormat;
 
 public class FormatNumber {
     public static void main(String[] args) {
+        //Getpercent instance
         String resultString = NumberFormat.getPercentInstance().format(0.74);
         System.out.println(resultString);
 
+
+        //Currency instance
         NumberFormat currency = NumberFormat.getCurrencyInstance();
         String result = currency.format(1266589.2354);
         System.out.println(result);
-
     }
 }
