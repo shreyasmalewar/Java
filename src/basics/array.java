a1e0261e3d6deb8edba8470d38bce71cef271aa5
shreyasmalewar <shreyasmalewar@gmail.com>
2020-05-04 01:19:41 +0530

    File packages updated app -> basics

---
+++ src/basics/array.java
@@ -0,0 +1,10 @@
+package basics;
+
+import java.util.Arrays;
+
+public class array {
+    public static void main(String[] args) {
+    int[][] numbers = { { 1, 2, 3}, { 4, 5, 6}};
+    System.out.println(Arrays.deepToString(numbers));
+    }
+}
---
+++ src/basics/strings.java
@@ -0,0 +1,25 @@
+package basics;
+
+public class strings {
+    public static void main(String[] args) {
+
+        String name = "Hey Shreyas";  //back slash is a special charecter
+        
+        System.out.println(name.endsWith("war"));
+        
+        System.out.println(name.startsWith("shr"));
+        
+        System.out.println(name.length());
+        
+        System.out.println(name.indexOf("eya"));
+        
+        System.out.println(name.replace("s", "S"));
+        System.out.println(name);
+        
+        System.out.println(name.toLowerCase());
+        
+        System.out.println(name.trim());
+        
+        System.out.println(name);
+    }
+}
