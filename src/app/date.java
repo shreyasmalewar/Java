be4ce5859fc06275faba393760cea267cc8e9789
shreyasmalewar <shreyasmalewar@gmail.com>
2020-05-03 19:20:02 +0530

    Date function revised

--- src/app/date.java
+++ src/app/date.java
@@ -2,11 +2,13 @@ package app;
 
 import java.util.Date;
 
-public class date {
+public class date{
     public static void main(String[] args) {
-        byte age = 32;
         Date today = new Date();
-        System.out.println(age);
+        byte age = 19;
+
+        System.out.printf("I turned %d on ", age);
         System.out.println(today);
+
     }
 }
