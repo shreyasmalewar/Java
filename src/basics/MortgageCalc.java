dfaa3fbb0e3ea381de999948365c4ffd30606a3a
shreyasmalewar <shreyasmalewar@gmail.com>
2020-05-05 21:37:30 +0530

    MortgageCal documented

--- src/basics/MortgageCalc.java
+++ src/basics/MortgageCalc.java
@@ -5,27 +5,29 @@ import java.util.Scanner;
 public class MortgageCalc {
     public static void main(String[] args) {
 
-        final byte MONTHS_IN_YEAR = 12;
-        final byte PERCENT = 100;
+        final byte MONTHS_IN_YEAR = 12; //FINAL for 12 months 
+        final byte PERCENT = 100;       //FINAL 100
 
-        Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in); //Scanner Class Implementation
 
-        System.out.println("Principal: ");
+        System.out.println("Principal: "); //Userinput for the value of Principal
         int principal = scanner.nextInt();
 
-        System.out.println("Annual rate of interest: ");
-        float annualInterest = scanner.nextFloat();
-
-        float monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
-        System.out.println("Period : ");
-
+        System.out.println("Annual rate of interest: "); //Userinput for Annual Rate of interest
+        float annualInterest = scanner.nextFloat();      //calculating annualInterest
+        float monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR; //Formula for calculating monthly interest
+        
+        
+        System.out.println("Period : "); //Userinput Number of years
         byte years = scanner.nextByte();
         int numberOfPayments = years * MONTHS_IN_YEAR;
 
+        //Formula for calculating mortgage using math lib
         double mortgage = principal
                             * (monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments))
                             / (Math.pow(1 + monthlyInterest, numberOfPayments) - 1);
 
+        //Displaying final mortgage amount
         System.out.println("Mortgage amount is: Rs");
         System.out.println(mortgage);
     }
