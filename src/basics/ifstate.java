ba7ceac7132a5c2fbd8f526d4423561b4df308d8
shreyasmalewar <shreyasmalewar@gmail.com>
2020-05-08 21:08:43 +0530

    created ifstate.java

---
+++ src/basics/ifstate.java
@@ -0,0 +1,18 @@
+package basics;
+
+public class ifstate {
+   public static void main(String[] args) {
+      int a = 4;
+      int b = 4;
+
+      if(a == b)
+      {
+          System.out.println("A and B are equal");
+      }
+
+      else
+      {
+          System.out.println("they aren't equal");
+      }
+   }
+}
