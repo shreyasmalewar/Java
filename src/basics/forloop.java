5343d6310f4fecd08ceaca928bb5503ddf4ad163
shreyasmalewar <shreyasmalewar@gmail.com>
2020-05-08 21:12:26 +0530

    Edited control statement

--- src/basics/forloop.java
+++ src/basics/forloop.java
@@ -4,9 +4,9 @@ public class forloop {
     public static void main(String[] args) {
         int i;
 
-        for(i = 0; i < 5; i++)
+        for(i = 1; i <= 5; i++)
         {
-            System.out.printf("%d iteration \n", ++i);
+            System.out.printf("%d iteration \n", i);
         }
     }
 }
