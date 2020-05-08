8354966a9c9e1587b10934449a73eb123b3fca25
shreyasmalewar <shreyasmalewar@gmail.com>
2020-05-08 21:08:32 +0530

    Created for loop.java

---
+++ src/basics/forloop.java
@@ -0,0 +1,12 @@
+package basics;
+
+public class forloop {
+    public static void main(String[] args) {
+        int i;
+
+        for(i = 0; i < 5; i++)
+        {
+            System.out.printf("%d iteration \n", i++);
+        }
+    }
+}
