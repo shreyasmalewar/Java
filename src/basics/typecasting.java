441a90c47328344b34674df51287054e274d2e71
shreyasmalewar <shreyasmalewar@gmail.com>
2020-05-08 21:25:54 +0530

    Implemented type casting

---
+++ src/basics/typecasting.java
@@ -0,0 +1,11 @@
+package basics;
+
+public class typecasting {
+    public static void main(String[] args) {
+        int a = 14;
+        float b = a;
+
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
