dac3c7bff0ebce7ea14e3ed70ae4614352e6a53a
shreyasmalewar <shreyasmalewar@gmail.com>
2020-05-09 23:06:17 +0530

    Optimised boolean

--- src/basics/ifstate.java
+++ src/basics/ifstate.java
@@ -3,13 +3,6 @@ package basics;
 public class ifstate {
 public static void main(String[] args) {
     int income = 120_000;
-    boolean hasHighIncome;
-    if(income > 100_000)
-        hasHighIncome = true;
-
-    else
-        hasHighIncome = false;
-
-    System.out.println(hasHighIncome);
+    boolean hasHighIncome = (income > 100_000);
 }
 }
