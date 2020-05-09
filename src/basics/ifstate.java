24b184ef3438bd738535b062e98640259ce281e8
shreyasmalewar <shreyasmalewar@gmail.com>
2020-05-09 23:32:49 +0530

    If state updated

--- src/basics/ifstate.java
+++ src/basics/ifstate.java
@@ -1,8 +1,22 @@
 package basics;
 
+import java.util.Scanner;
+
 public class ifstate {
-public static void main(String[] args) {
-    int income = 120_000;
-    boolean hasHighIncome = (income > 100_000);
-}
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        if(a ==b)
+        {
+            System.out.printf("%d is equal to %d", a, b);
+        }
+
+        else
+        {
+            System.out.printf("%d is not equal to %d", a, b);
+        }
+    }
 }
