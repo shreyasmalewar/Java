14ef6fba6289ed12b1e02441d98bef4ce3fb6fbf
shreyasmalewar <shreyasmalewar@gmail.com>
2020-05-09 22:30:01 +0530

    Comments added

--- src/basics/ifeleif.java
+++ src/basics/ifeleif.java
@@ -2,18 +2,23 @@ package basics;
 
 public class ifeleif {
     public static void main(String[] args) {
+        //initialization
         int t = 32;
+
+        //if statement
         if(t > 30)
         {
             System.out.println("It is a hot day");
             System.out.println("Drink plenty of water");
         }
 
+        //Else if statement
         else if(t > 20 && t <= 30)
         {
             System.out.println("Beautiful day");
         }
 
+        //Else statement
         else
         {
             System.out.println("Cold Day");
