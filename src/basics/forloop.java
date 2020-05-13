e3c2bbbd8627add49b99c99e8c9ed0baa0febc66
shreyasmalewar <shreyasmalewar@gmail.com>
2020-05-13 23:16:16 +0530

    while loop added

--- src/basics/forloop.java
+++ src/basics/forloop.java
@@ -2,11 +2,16 @@ package basics;
 
 public class forloop {
     public static void main(String[] args) {
-        int i;
+        //int i;
 
-        for(i = 1; i <= 5; i++)
+        for(int i = 1; i <= 5; i++)
         {
             System.out.printf("%d iteration \n", + i);
         }
+
+        int i = 5;
+        while (i > 0)
+            System.out.println("Hello Java " + i);
+            i--;
     }
 }
