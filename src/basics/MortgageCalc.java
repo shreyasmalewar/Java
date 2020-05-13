1e4e073ed101e64ee03de573eb93d65875a281f0
shreyasmalewar <shreyasmalewar@gmail.com>
2020-05-13 23:03:51 +0530

    Resource leak and bugs fixed

--- src/basics/MortgageCalc.java
+++ src/basics/MortgageCalc.java
@@ -5,43 +5,38 @@ import java.util.Scanner;
 public class MortgageCalc {
     public static void main(String[] args) {
 
-        final byte MONTHS_IN_YEAR = 12; //FINAL for 12 months 
-        final byte PERCENT = 100;       //FINAL 100
+        final byte MONTHS_IN_YEAR = 12; // FINAL for 12 months
+        final byte PERCENT = 100; // FINAL 100
+        byte years;
+        int principal;
+        int numberOfPayments;
+        float annualInterest;
+        float monthlyInterest;
 
+        // Scanner Class Implementation
+        Scanner scanner = new Scanner(System.in);
 
-        //Scanner Class Implementation
-        Scanner scanner = new Scanner(System.in); 
+        try {
+            System.out.printf("Principal: ");
+            principal = scanner.nextInt();
 
+            System.out.printf("Annual interest rate: ");
+            annualInterest = scanner.nextFloat();
+            monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
 
-        //Userinput for the value of Principal
-        System.out.println("Principal: "); 
-        int principal = scanner.nextInt();
+            System.out.printf("Period: ");
+            years = scanner.nextByte();
+            numberOfPayments = years * MONTHS_IN_YEAR;
+        } finally {
+            scanner.close();
+        }
 
+        // Formula for calculating mortgage using math lib
+        double mortgage = principal * (monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments))
+                / (Math.pow(1 + monthlyInterest, numberOfPayments) - 1);
 
-        //Userinput for Annual Rate of interest
-        System.out.println("Annual rate of interest: "); 
-
-
-        //Formula for calculating monthly interest
-        float annualInterest = scanner.nextFloat();      
-        float monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR; 
-        
-
-        //Userinput Number of years
-        System.out.println("Period : "); 
-        byte years = scanner.nextByte();
-        int numberOfPayments = years * MONTHS_IN_YEAR;
-
-
-        //Formula for calculating mortgage using math lib
-        double mortgage = principal
-                            * (monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments))
-                            / (Math.pow(1 + monthlyInterest, numberOfPayments) - 1);
-
-                            
-        //Displaying final mortgage amount
-        System.out.println("Mortgage amount is: Rs");
+        // Displaying final mortgage amount
+        System.out.printf("Mortgage amount is: Rs");
         System.out.println(mortgage);
     }
 }
-
