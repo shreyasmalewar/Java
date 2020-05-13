999791cd606b64d9c1a55ced64238859a0e481b4
shreyasmalewar <shreyasmalewar@gmail.com>
2020-05-13 16:58:09 +0530

    Numeric implementation of switchcase

--- src/basics/switchcase.java
+++ src/basics/switchcase.java
@@ -1,16 +1,31 @@
 package basics;
 
+import java.util.Scanner;
+
 public class switchcase {
     public static void main(String[] args) {
-        String role = "admin";
+        int role;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("1. Admin");
+        System.out.println("2. Moderator");
+        System.out.println("3. Guest");
+        System.out.println("Enter your choice");
+
+        try{
+            role = scanner.nextInt();
+        }finally {
+            scanner.close();
+        }
         
         switch (role) 
         {
-            case "admin":
+            case 1:
                 System.out.println("You are an admin");
             break;
 
-            case "moderator":
+            case 2:
                 System.out.println("You are a moderator");
             break;
 
