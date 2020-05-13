0e956f957bdae32126c62c5abc54bdc8989135ae
shreyasmalewar <shreyasmalewar@gmail.com>
2020-05-13 16:45:35 +0530

    Switch case implementation using string

---
+++ src/basics/switchcase.java
@@ -0,0 +1,22 @@
+package basics;
+
+public class switchcase {
+    public static void main(String[] args) {
+        String role = "admin";
+        
+        switch (role) 
+        {
+            case "admin":
+                System.out.println("You are an admin");
+            break;
+
+            case "moderator":
+                System.out.println("You are a moderator");
+            break;
+
+            default:
+                System.out.println("You are a Guest");
+            break;
+        }
+    }
+}
