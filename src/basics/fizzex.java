3d46d0afb0216a16d7c832f6f1697b749dbaaf3d
shreyasmalewar <shreyasmalewar@gmail.com>
2020-05-13 23:11:17 +0530

    Quality upgrade

--- src/basics/fizzex.java
+++ src/basics/fizzex.java
@@ -6,7 +6,7 @@ public class fizzex {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int number;
-        System.out.println("Number: ");
+        System.out.printf("Number: ");
 
         try {
             number = scanner.nextInt();
@@ -14,14 +14,14 @@ public class fizzex {
             scanner.close();
         }
 
-        if (number % 5 == 0)
+        if (number % 5 == 0 && number % 3 == 0)
+            System.out.println("FizzBuzz");
+
+        else if (number % 5 == 0)
             System.out.println("Fizz");
 
         else if (number % 3 == 0)
             System.out.println("Buzz");
-
-        else if (number % 5 == 0)
-            System.out.println("Fizzbuzz");
 
         else
             System.out.println(number);
