fdfe01bdb096fff8d148a275241f75831d086741
shreyasmalewar <shreyasmalewar@gmail.com>
2020-06-02 20:19:09 +0530

    2D Array

---
+++ src/practicesession/TwoDAddition.java
@@ -0,0 +1,26 @@
+package practicesession;
+
+public class TwoDAddition {
+    public static void main(String[] args) {
+        int rows = 2, columns = 4;
+
+        // Declaring the two matrices as multi-dimensional arrays
+        int[][] MatrixA = { { 1, 1, 1, 1 }, { 2, 3, 5, 2 } };
+        int[][] MatrixB = { { 2, 3, 4, 5 }, { 2, 2, 4, -4 } };
+
+        int[][] sum = new int[rows][columns];
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                sum[i][j] = MatrixA[i][j] + MatrixB[i][j];
+            }
+        }
+        // Displaying the sum matrix
+        System.out.println("Sum of the given matrices is: ");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                System.out.print(sum[i][j] + "    ");
+            }
+            System.out.println();
+        }
+    }
+}
