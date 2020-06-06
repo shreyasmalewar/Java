e21001d0800fb6d7b1c0fcc4ee1ed9725a5cd7d2
shreyasmalewar <shreyasmalewar@gmail.com>
2020-06-06 14:49:17 +0530

    added userinput

---
+++ src/practicesession/UserInput.java
@@ -0,0 +1,28 @@
+
+package practicesession;
+
+import java.util.Scanner;
+
+public class UserInput {
+  public static void main(String[] args) {
+
+    String name;
+
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.print("Name :");
+
+    try {
+
+      name = scanner.nextLine().trim();
+
+    } finally {
+
+      scanner.close();
+
+    }
+
+    System.out.println("Okay! You are " + name);
+  }
+
+}
