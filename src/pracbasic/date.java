8e59ba773f0dcbd9338a651c9106e46fc0e5a630
shreyasmalewar <shreyasmalewar@gmail.com>
2020-06-11 19:50:36 +0530

    added date.java

---
+++ src/pracbasic/date.java
@@ -0,0 +1,14 @@
+package pracbasic;
+
+import java.util.Date;
+
+public class date{
+    public static void main(String[] args) {
+        Date today = new Date();
+        byte age = 19;
+
+        System.out.printf("I turned %d on ", age);
+        System.out.println(today);
+
+    }
+}
