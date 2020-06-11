9d1de85a8facfdddbdb03e5b8200d48279c3d28f
shreyasmalewar <shreyasmalewar@gmail.com>
2020-06-11 19:51:57 +0530

    added typecasting

---
+++ src/pracbasic/typecasting.java
@@ -0,0 +1,14 @@
+package pracbasic;
+
+public class typecasting {
+    public static void main(String[] args) {
+        int a = 14;
+        float b = a;
+
+        //prints as integer
+        System.out.println(a);
+
+        //prints as float
+        System.out.println(b);
+    }
+}
