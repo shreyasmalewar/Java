202120eb9efdab697af1714ea70012ba8b321118
shreyasmalewar <shreyasmalewar@gmail.com>
2020-06-22 10:33:35 +0530

    cleanup practice sessions

--- src/chatbot/simplebot.java
+++
@@ -1,8 +0,0 @@
-package chatbot;
-
-public class simplebot {
-    public static void main(String[] args) {
-        System.out.println("Hello! My name is Aid.");
-        System.out.println("I was created in 2020.");
-    }
-}
