e6867eca20f6c62b7f79eaff1637f625bd1cd211
shreyasmalewar <shreyasmalewar@gmail.com>
2020-07-14 20:48:07 +0530

    added userinput JBAcademy

---
+++ src/jetbrainsacademy/userinput.java
@@ -0,0 +1,22 @@
+package jetbrainsacademy;
+
+import java.util.Scanner;
+
+public class userinput {
+   public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    String word1 = scanner.nextLine();
+    String word2 = scanner.nextLine();
+    String word3 = scanner.nextLine();
+    scanner.close();
+
+    System.out.println(word3);
+    System.out.println(word2);
+    System.out.println(word1);
+
+    //java pro language implemented
+    //https://hyperskill.org/learn/step/2922
+   }
+
+}
