cc3e81f672a74c04a313bfaeef7f53750a602aa0
shreyasmalewar <shreyasmalewar@gmail.com>
2020-07-15 22:54:15 +0530

    :lipstick: jetbrains code quality test

--- src/jetbrainsacademy/numberofbench.java
+++ src/jetbrainsacademy/numberofbench.java
@@ -2,34 +2,36 @@ package jetbrainsacademy;
 
 import java.util.Scanner;
 
-public class numberofbench {
+class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int classOne = scanner.nextInt();
         int classTwo = scanner.nextInt();
         int classThree = scanner.nextInt();
+
+        scanner.close();
+
+        //number of benches in class 1, 2 and 3
         int benchesOne;
         int benchesTwo;
         int benchesThree;
 
-        scanner.close();;
-
-        if(classOne % 2 == 0) {
+        if (classOne % 2 == 0) {
             benchesOne = classOne / 2;
-        }else{
-            benchesOne = (classOne / 2) + 1;
+        } else {
+            benchesOne = classOne / 2 + 1;
         }
 
-        if(classTwo % 2 == 0) {
+        if (classTwo % 2 == 0) {
             benchesTwo = classTwo / 2;
-        }else{
-            benchesTwo = (classTwo / 2) + 1;
+        } else {
+            benchesTwo = classTwo / 2 + 1;
         }
 
-        if(classThree % 2 == 0) {
+        if (classThree % 2 == 0) {
             benchesThree = classThree / 2;
-        }else{
-            benchesThree = (classThree / 2) + 1;
+        } else {
+            benchesThree = classThree / 2 + 1;
         }
 
         int totalBenches = benchesOne + benchesTwo + benchesThree;
