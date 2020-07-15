a5cc06549674e174b8ee9495727b92aac386bf66
shreyasmalewar <shreyasmalewar@gmail.com>
2020-07-15 23:45:33 +0530

    added numreverse.java

---
+++ src/jetbrainsacademy/numreverse.java
@@ -0,0 +1,24 @@
+package jetbrainsacademy;
+
+import java.util.Scanner;
+
+public class numreverse {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int number = scanner.nextInt();
+        int remainder;
+        int reverse = 0;
+    
+        scanner.close();
+
+        while (number > 0) {
+            remainder = number % 10;
+            reverse = (reverse * 10) + remainder;
+            number = number / 10;
+        }
+
+        System.out.println(reverse);
+            
+    }
+}
