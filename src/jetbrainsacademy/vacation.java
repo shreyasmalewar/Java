968bb2d694d0d7ef497d5cb13ea8a63fd8365ba5
shreyasmalewar <shreyasmalewar@gmail.com>
2020-07-16 00:04:24 +0530

    added vacation.java solution

---
+++ src/jetbrainsacademy/vacation.java
@@ -0,0 +1,25 @@
+package jetbrainsacademy;
+
+import java.util.Scanner;
+
+public class vacation {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    
+        int numberOfDays = scanner.nextInt();
+        int foodCostPerDay = scanner.nextInt();
+        int flightCost = scanner.nextInt();
+        int hotelCostPerNight = scanner.nextInt();
+
+        scanner.close();
+    
+        int totalFoodCost = foodCostPerDay * numberOfDays;
+        int totalFlightCost = flightCost * 2;
+        int totalHotelCost = hotelCostPerNight * (numberOfDays - 1);
+    
+        int totalTripCost = totalFlightCost + totalHotelCost + totalFoodCost;
+
+        System.out.println(totalTripCost);
+    
+    }
+}
