bd8cce40e421f369ba608e6fee72740fe7e392ed
shreyasmalewar <shreyasmalewar@gmail.com>
2020-07-17 09:59:53 +0530

    :pencil: substrings

--- src/jetbrainsacademy/substrings.java
+++ src/jetbrainsacademy/substrings.java
@@ -5,12 +5,15 @@ import java.util.Scanner;
 public class substrings {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        //start and end parameters are used to extract substring
         String str = scanner.nextLine();
         int start = scanner.nextInt();
         int end = scanner.nextInt();
 
         scanner.close();
         
+        //substring implemented
         str = str.substring(start, end);
         System.out.println(str);
     }
