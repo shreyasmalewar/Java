2c339410a4a167f5e164dcafd836db288e3ad29c
shreyasmalewar <shreyasmalewar@gmail.com>
2020-07-17 09:56:13 +0530

    added stringcomp.java

---
+++ src/jetbrainsacademy/stringcompare.java
@@ -0,0 +1,17 @@
+package jetbrainsacademy;
+
+import java.util.Scanner;
+
+public class stringcompare {
+    public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    String a = scanner.nextLine();
+    String b = scanner.nextLine();
+    
+    scanner.close();
+    a = a.replace(" ", "");
+    b = b.replace(" ", "");
+    System.out.println(b.equals(a));
+    }
+}
