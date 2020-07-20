e960f5239f510572cf753538392d94cf402b59ec
shreyasmalewar <shreyasmalewar@gmail.com>
2020-07-20 18:48:11 +0530

    ++ true if positive

---
+++ src/jetbrainsacademy/bool/trueifpositive.java
@@ -0,0 +1,20 @@
+package jetbrainsacademy.bool;
+
+import java.util.Scanner;
+
+public class trueifpositive {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+        scanner.close();
+
+        if ((a > 0 && b <= 0 && c <= 0) || (a <= 0 && b > 0 && c <= 0) || (a <= 0 && b <= 0 && c > 0)) {
+            System.out.println(true);
+        } else {
+            System.out.println(false);
+        }
+    }
+}
