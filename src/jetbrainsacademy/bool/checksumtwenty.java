ac86f4d63c10065c365726b0ecde240ffc26f16e
shreyasmalewar <shreyasmalewar@gmail.com>
2020-07-20 18:33:25 +0530

    added checksum20

---
+++ src/jetbrainsacademy/bool/checksumtwenty.java
@@ -0,0 +1,20 @@
+package jetbrainsacademy.bool;
+
+import java.util.Scanner;
+
+public class checksumtwenty {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+        scanner.close();
+
+        if ( a + b == 20 || a + c == 20) {
+            System.out.println(true);
+        } else {
+            System.out.println(false);
+        }
+    }
+}
