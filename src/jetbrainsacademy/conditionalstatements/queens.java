12c9d5edb68e8f1901a479ed4070bd730d5143bf
shreyasmalewar <shreyasmalewar@gmail.com>
2020-07-21 23:32:11 +0530

    :sparkles: queens.java

---
+++ src/jetbrainsacademy/conditionalstatements/queens.java
@@ -0,0 +1,17 @@
+package jetbrainsacademy.conditionalstatements;
+
+import java.util.Scanner;
+
+import javax.swing.plaf.synth.SynthScrollBarUI;
+
+public class queens {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int x1 = Math.abs(scanner.nextInt());
+        int y1 = Math.abs(scanner.nextInt());
+        int x2 = Math.abs(scanner.nextInt());
+        int y2 = Math.abs(scanner.nextInt());
+
+        
+    }
+}
