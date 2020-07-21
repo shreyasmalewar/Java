366514ff4ec72f14b3738f58e02c8b1bfb32229c
shreyasmalewar <shreyasmalewar@gmail.com>
2020-07-21 23:16:25 +0530

    :wrench: resource leak

--- src/jetbrainsacademy/conditionalstatements/intervalnum.java
+++ src/jetbrainsacademy/conditionalstatements/intervalnum.java
@@ -6,6 +6,7 @@ public class intervalnum {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int number = scanner.nextInt();
+        scanner.close();
 
         if ((-15 < number && number <= 12) || (14 < number && number < 17) || (19 <= number)) {
             System.out.println("True");
