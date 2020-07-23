523ac64e1ca64c82a7e558a63b56c06d65156862
shreyasmalewar <shreyasmalewar@gmail.com>
2020-07-23 22:42:27 +0530

    :pencil: explained digit calculations

--- src/jetbrainsacademy/conditionalstatements/symetricalnumber.java
+++ src/jetbrainsacademy/conditionalstatements/symetricalnumber.java
@@ -9,9 +9,9 @@ public class symetricalnumber {
         int number = scanner.nextInt();
         scanner.close();
 
-        int n1 = number % 10;
-        int n2 = (number / 10) % 10;
-        int n3 = (number / 100) % 10;
+        int n1 = number % 10; //for fourth digit
+        int n2 = (number / 10) % 10; //for third digit
+        int n3 = (number / 100) % 10; //for second digit
         int n4 = (number / 1000) % 10;
 
         if (n1 == n4 && n2 == n3) {
