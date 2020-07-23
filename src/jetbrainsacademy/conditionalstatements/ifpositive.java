69ae2026c3c8d536e50bb64ec4de635980871cee
shreyasmalewar <shreyasmalewar@gmail.com>
2020-07-23 21:58:11 +0530

    :sparkles: if positive

---
+++ src/jetbrainsacademy/conditionalstatements/ifpositive.java
@@ -0,0 +1,17 @@
+package jetbrainsacademy.conditionalstatements;
+
+import java.util.Scanner;
+
+public class ifpositive {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        scanner.close();
+
+        if (number >= 0) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+    }
+}
