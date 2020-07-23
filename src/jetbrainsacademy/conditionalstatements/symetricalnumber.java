e9274e15c9f8da858d8d9856ff03a6a3e72139a5
shreyasmalewar <shreyasmalewar@gmail.com>
2020-07-23 22:38:06 +0530

    :sparkles: symetrical num problem

---
+++ src/jetbrainsacademy/conditionalstatements/symetricalnumber.java
@@ -0,0 +1,23 @@
+package jetbrainsacademy.conditionalstatements;
+
+import java.util.Scanner;
+
+public class symetricalnumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int number = scanner.nextInt();
+        scanner.close();
+
+        int n1 = number % 10;
+        int n2 = (number / 10) % 10;
+        int n3 = (number / 100) % 10;
+        int n4 = (number / 1000) % 10;
+
+        if (n1 == n4 && n2 == n3) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+    }
+}
