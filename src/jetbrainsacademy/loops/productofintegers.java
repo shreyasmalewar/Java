9818a559cd9eeed2a1d36ff66318a5b68327eca0
shreyasmalewar <shreyasmalewar@gmail.com>
2020-07-25 20:58:37 +0530

    :sparkles:

---
+++ src/jetbrainsacademy/loops/productofintegers.java
@@ -0,0 +1,21 @@
+package jetbrainsacademy.loops;
+
+import java.util.Scanner;
+
+public class productofintegers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        long product = 1;
+
+        scanner.close();
+
+        for (int i = a; i < b; i++) {
+            product = product * i;
+        }
+        
+        System.out.println(product);
+    }
+}
