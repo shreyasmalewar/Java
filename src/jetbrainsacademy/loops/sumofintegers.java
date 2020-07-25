f4126f5fabb6658b868a523299cce9a374d3c885
shreyasmalewar <shreyasmalewar@gmail.com>
2020-07-25 20:45:18 +0530

    :sparkles: sum of integers in an interval

---
+++ src/jetbrainsacademy/loops/sumofintegers.java
@@ -0,0 +1,19 @@
+package jetbrainsacademy.loops;
+
+import java.util.Scanner;
+
+public class sumofintegers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int sum = 0;
+        scanner.close();
+
+        for (int i = a; i <= b; i++) {
+            sum = sum + i;
+        }
+        System.out.println(sum);
+    }
+}
