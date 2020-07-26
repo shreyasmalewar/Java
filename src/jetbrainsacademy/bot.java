c40bab6112700c5de67c622fee149e6ec7b8285c
shreyasmalewar <shreyasmalewar@gmail.com>
2020-07-26 22:25:41 +0530

    bot updated with 3rd assignment

--- src/jetbrainsacademy/bot.java
+++ src/jetbrainsacademy/bot.java
@@ -21,10 +21,22 @@ public class bot {
         int remainderThree = scanner.nextInt();
         int remainderFive = scanner.nextInt();
         int remainderSeven = scanner.nextInt();
-        scanner.close();
+        
         
         int age = (remainderThree * 70 + remainderFive * 21 + remainderSeven * 15) % 105;
 
         System.out.println("Your age is " + age + "; that's a good time to start programming!");
+
+        //counting ability
+        System.out.println("Now I will prove to you that I can count to any number you want.");
+
+        int count = scanner.nextInt();
+        scanner.close();
+
+        for (int i = 1; i <= count; i++) {
+            System.out.println(i + "\n");
+        }
+
+        System.out.println("Completed, have a nice day!");
     }
 }
