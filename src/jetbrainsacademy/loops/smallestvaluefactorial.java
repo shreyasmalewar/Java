34c6d67105300018f36ded01bec3384ce7d87e8c
shreyasmalewar <shreyasmalewar@gmail.com>
2020-07-29 22:04:02 +0530

    :construction: needs additional help

---
+++ src/jetbrainsacademy/loops/smallestvaluefactorial.java
@@ -0,0 +1,12 @@
+package jetbrainsacademy.loops;
+
+import java.util.Scanner;
+
+public class smallestvaluefactorial {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        long number = scanner.nextInt();
+        int i;
+        
+    }
+}
