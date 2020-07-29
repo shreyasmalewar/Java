ab1e781301ebc037113c372d02ebb76088171561
shreyasmalewar <shreyasmalewar@gmail.com>
2020-07-29 20:46:41 +0530

    :sparkles: square of naturals

---
+++ src/jetbrainsacademy/loops/SquareofNarurals.java
@@ -0,0 +1,18 @@
+package jetbrainsacademy.loops;
+
+import java.util.Scanner;
+
+public class SquareofNarurals {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        int number = scanner.nextInt();
+        int i = 1;
+        scanner.close();
+
+        while (i * i < number) {
+            System.out.println(i * i);
+            i++;
+        }
+    }
+}
