7f5ed459ca1323a97b5db1be12eed5b13129f06f
shreyasmalewar <shreyasmalewar@gmail.com>
2020-07-30 13:26:30 +0530

    :sparkles: bus tour problem and config file

---
+++ src/jetbrainsacademy/branchstatements/bustour.java
@@ -0,0 +1,28 @@
+package jetbrainsacademy.branchstatements;
+
+import java.util.Scanner;
+
+public class bustour {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int busHeight = scanner.nextInt();
+        int numberOfBridges = scanner.nextInt();
+        int heightOfBridge;
+        boolean crash = false;
+        int i = 0;
+
+        for (i = 0; i < numberOfBridges; i++) {
+            heightOfBridge = scanner.nextInt();
+            if (busHeight >= heightOfBridge) {
+                crash = true;
+                System.out.println("Will crash on bridge " + (i + 1));
+                break;
+            } 
+        }
+
+        if (crash == false) {
+            System.out.println("Will not crash");
+        }
+    }
+}
