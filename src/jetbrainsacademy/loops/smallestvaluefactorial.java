cd5c46be9ec36d70ac3727eba48d42c3339d37df
shreyasmalewar <shreyasmalewar@gmail.com>
2020-07-30 09:49:59 +0530

    removed warnings lol

--- src/jetbrainsacademy/loops/smallestvaluefactorial.java
+++ src/jetbrainsacademy/loops/smallestvaluefactorial.java
@@ -5,8 +5,9 @@ import java.util.Scanner;
 public class smallestvaluefactorial {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        long number = scanner.nextInt();
-        int i;
+        //long number = scanner.nextInt();
+       // int i;
+        scanner.close();
         
     }
 }
