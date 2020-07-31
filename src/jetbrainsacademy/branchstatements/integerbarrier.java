3ec8dc36c05edac5be1ded2795550ca22ae91742
shreyasmalewar <shreyasmalewar@gmail.com>
2020-07-31 09:38:33 +0530

    added integer barrier and config

---
+++ src/jetbrainsacademy/branchstatements/integerbarrier.java
@@ -0,0 +1,28 @@
+package jetbrainsacademy.branchstatements;
+
+import java.util.Scanner;
+
+public class integerbarrier {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int sum = 0;
+        int number;
+
+        while (scanner.hasNext()) {
+            number = scanner.nextInt();
+
+            if (number == 0) {
+                System.out.println(sum);
+                break;
+            } else {
+                sum = sum + number;
+            }
+
+            if (sum >= 1000) {
+                System.out.println(sum - 1000);
+                break;
+            }     
+        }
+    }
+}
