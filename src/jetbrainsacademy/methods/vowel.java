c5903ffb1a2e3c7419c3d371e6df0b05521bf3ca
Shreyas <shreyasmalewar@gmail.com>
2020-08-04 16:15:50 +0530

    :sparkles: vowel.java and config

---
+++ src/jetbrainsacademy/methods/vowel.java
@@ -0,0 +1,22 @@
+package jetbrainsacademy.methods;
+
+import java.util.Scanner;
+
+public class vowel {
+
+    public static boolean isVowel(char ch) {
+        ch = Character.toLowerCase(ch);
+        if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        char letter = scanner.nextLine().charAt(0);
+        scanner.close();
+        System.out.println(isVowel(letter) ? "YES" : "NO");
+    }
+}
