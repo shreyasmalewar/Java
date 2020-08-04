70f0ed0bc3a31c7e404371746a1f83dc2465066c
Shreyas <shreyasmalewar@gmail.com>
2020-08-04 12:42:23 +0530

    :sparkles: factorial program methods, config file

---
+++ src/jetbrainsacademy/methods/factorial.java
@@ -0,0 +1,22 @@
+package jetbrainsacademy.methods;
+
+import java.util.Scanner;
+
+public class factorial {
+
+    public static long factorialFunc(long n) {
+        long fact = 1;
+        for (int i = 0; i < n; i++) {
+            fact = fact * i + fact; 
+        }
+        return fact;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        long n = Integer.parseInt(scanner.nextLine().trim());
+        scanner.close();
+
+        System.out.println(factorialFunc(n));
+    }
+}
