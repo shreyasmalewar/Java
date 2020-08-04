b7c555ac7a61d334e201a277d24d79c2214feab8
Shreyas <shreyasmalewar@gmail.com>
2020-08-04 16:43:59 +0530

    :sparkles: signofnum and config

---
+++ src/jetbrainsacademy/methods/signofnum.java
@@ -0,0 +1,23 @@
+package jetbrainsacademy.methods;
+
+import java.util.Scanner;
+
+public class signofnum {
+
+    public static int sign(int number) {
+        if (number >= 1) {
+            return 1;
+        } else if (number <= -1) {
+            return -1;
+        } else {
+            return 0;
+        }
+    }
+
+    public static void main(String[] args) {
+        final Scanner scanner = new Scanner(System.in);
+        final int n = scanner.nextInt();
+        scanner.close();
+        System.out.println(sign(n));
+    }
+}
