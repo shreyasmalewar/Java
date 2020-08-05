96f2d7c836d065ef04646a87df0b81622168312d
Shreyas <shreyasmalewar@gmail.com>
2020-08-05 12:20:09 +0530

    modularity added, major refactoring

--- src/jetbrainsacademy/bot.java
+++ src/jetbrainsacademy/bot.java
@@ -3,31 +3,40 @@ package jetbrainsacademy;
 import java.util.Scanner;
 
 public class bot {
+    final static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        greet("Aid", "2020");
+        remindName();
+        guessAge();
+        count();
+        end();
+    }
 
-        System.out.println("Hello! My name is Aid.");
-        System.out.println("I was created in 2018.");
+    static void greet (String assistantName, String birthYear) {
+        System.out.println("Hello! My name is" + assistantName + ".");
+        System.out.println("I was created in 2018" + assistantName + ".");
         System.out.println("Please, remind me your name.");
+    }
 
-        // reading a name
+    static void remindName () {
         String name = scanner.nextLine();
-
         System.out.println("What a great name you have, " + name + "!");
+    }
+
+    static void guessAge () {
         System.out.println("Let me guess your age.");
         System.out.println("Enter remainders of dividing your age by 3, 5 and 7.");
 
-        // reading all remainders
         int remainderThree = scanner.nextInt();
         int remainderFive = scanner.nextInt();
         int remainderSeven = scanner.nextInt();
         
-        
         int age = (remainderThree * 70 + remainderFive * 21 + remainderSeven * 15) % 105;
 
         System.out.println("Your age is " + age + "; that's a good time to start programming!");
+    }
 
-        //counting ability
+    static void count () {
         System.out.println("Now I will prove to you that I can count to any number you want.");
 
         int count = scanner.nextInt();
@@ -36,7 +45,13 @@ public class bot {
         for (int i = 0; i <= count; i++) {
             System.out.println(i + "\n");
         }
+    }
 
+    static void test() {
+        //mycode
+    }
+
+    static void end() {
         System.out.println("Completed, have a nice day!");
     }
 }
