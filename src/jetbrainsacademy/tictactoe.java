94eadeb3216312960e715509207ce4a3236f4888
Shreyas <shreyasmalewar@gmail.com>
2020-08-05 15:24:36 +0530

    :sparkles: new project tic tac

---
+++ src/jetbrainsacademy/tictactoe.java
@@ -0,0 +1,9 @@
+package jetbrainsacademy;
+
+public class tictactoe {
+    public static void main(String[] args) {
+        System.out.println("X O X");
+        System.out.println("O X X");
+        System.out.println("O O X");
+    }
+}
