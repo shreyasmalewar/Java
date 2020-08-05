1fd02d7c89e537d30194d747a3a984834703df31
Shreyas <shreyasmalewar@gmail.com>
2020-08-05 16:28:07 +0530

    :construction:

--- src/jetbrainsacademy/tictactoe.java
+++ src/jetbrainsacademy/tictactoe.java
@@ -1,9 +1,11 @@
 package jetbrainsacademy;
 
+
+import java.util.Scanner;
+
 public class tictactoe {
     public static void main(String[] args) {
-        System.out.println("X O X");
-        System.out.println("O X X");
-        System.out.println("O O X");
+        Scanner scanner = new Scanner(System.in);
+        scanner.close();
     }
 }
