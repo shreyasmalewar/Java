dbe055a9172178def26d285168f59354791889b1
Shreyas <shreyasmalewar@gmail.com>
2020-08-06 22:16:35 +0530

    :sparkles: boxproblem

---
+++ src/jetbrainsacademy/array/boxproblem.java
@@ -0,0 +1,35 @@
+package jetbrainsacademy.array;
+
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class boxproblem {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int[] box1 = new int[3];
+        int[] box2 = new int[3];
+
+        for (int i = 0; i < 3; i++) {
+            box1[i] = scanner.nextInt();
+        }
+        for (int i = 0; i < 3; i++) {
+            box2[i] = scanner.nextInt();
+        }
+
+        scanner.close();
+        Arrays.sort(box1);
+        Arrays.sort(box2);
+
+        if (box1[0] == box2[0] && box1[1] == box2[1] && box1[2] == box2[2]) {
+            System.out.println("Box 1 = Box 2");    
+        } else if (box1[0] >= box2[0] && box1[1] >= box2[1] && box1[2] >= box2[2]) {
+            System.out.println("Box 1 > Box 2");
+        } else if (box1[0] <= box2[0] && box1[1] <= box2[1] && box1[2] <= box2[2]) {
+            System.out.println("Box 1 < Box 2");
+        } else {
+            System.out.println("Incomparable");
+        }
+    }
+}
