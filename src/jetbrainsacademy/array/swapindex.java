e6fd6b9c46464c0280c886a8a2c64baee7716e73
Shreyas <shreyasmalewar@gmail.com>
2020-08-07 23:53:14 +0530

    test case 02 passed, subroutines removed

--- src/jetbrainsacademy/array/swapindex.java
+++ src/jetbrainsacademy/array/swapindex.java
@@ -3,55 +3,36 @@ package jetbrainsacademy.array;
 import java.util.Scanner;
 
 public class swapindex {
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int row = scanner.nextInt();
+        int column = scanner.nextInt();
 
-        int n = scanner.nextInt();
-        int m = scanner.nextInt();
+        int[][] matrix = new int[row][column];
+        int[][] swap = matrix.clone();
 
-        int[][] array = new int[n][m];
-
-        // insert elements
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                array[i][j] = scanner.nextInt();
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < column; j++) {
+                matrix[i][j] = scanner.nextInt();
             }
         }
 
-        System.out.println("elements are:");
-       // disp elements
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                System.out.printf(array[i][j] + " ");
-            }System.out.println();
-        }
-
-        swap(n, m, array);
-        
-        
-        scanner.close();
-    }
-
-    static void swap (int n, int m, int[][] array) {
-        System.out.println("function entered");
-        Scanner scanner = new Scanner(System.in);
-        int x = scanner.nextInt();
-        int y = scanner.nextInt();
-        System.out.println("input complete");
+        int c0 = scanner.nextInt();
+        int c1 = scanner.nextInt();
         scanner.close();
 
-        for (int i = 0; i < m; i++) {
-            int temp = array[i][x];
-            array[i][x] = array[i][y];
-            array[i][y] = temp;
+        for (int i = 0; i < row; i++) {
+            int temp = matrix[i][c0];
+            matrix[i][c0] = matrix[i][c1];
+            matrix[i][c1] = temp;
         }
 
-        System.out.println("printing array");
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                System.out.printf(array[i][j] + " ");
-            }System.out.println();
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < column; j++) {
+                System.out.print(swap[i][j] + " ");
+            }
+            System.out.println();
         }
-
     }
 }
