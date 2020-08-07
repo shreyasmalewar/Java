af64a228187e0aa150b78fef144e70ee93b88bdb
Shreyas <shreyasmalewar@gmail.com>
2020-08-07 20:46:23 +0530

    :construction: taxes.java

---
+++ src/jetbrainsacademy/array/taxes.java
@@ -0,0 +1,47 @@
+package jetbrainsacademy.array;
+
+import java.util.Scanner;
+
+public class taxes {
+
+    final static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        inputData();
+    }
+
+    static void inputData() {
+        int numberOfCompanies = scanner.nextInt();
+        int[] yearlyIncomes = new int[numberOfCompanies];
+        int[] individualTax = new int[numberOfCompanies];
+
+        //loop to enter yearly income
+        for (int i = 0; i < yearlyIncomes.length; i++) {
+            yearlyIncomes[i] = scanner.nextInt();
+        }
+
+        //loop to enter individual tax
+        for (int i = 0; i < individualTax.length; i++) {
+            individualTax[i] = scanner.nextInt();
+        }
+
+        maxTax(numberOfCompanies, yearlyIncomes, individualTax);
+    }
+
+    static void maxTax (int numberOfCompanies, int[] yearlyIncomes, int[] individualTax) {
+        double[] taxes = new double [numberOfCompanies];
+        for (int i = 0; i < yearlyIncomes.length; i++) {
+            taxes[i] = (yearlyIncomes[i] *individualTax[i]) / 100;
+        }
+
+        int maxPos = 1;
+        int max = 0;
+        for (int i = 1; i < numberOfCompanies; i++) {
+            if (taxes[i] > max) {
+                maxPos = i;
+            }
+        }
+
+        System.out.println(maxPos);
+    }
+}
