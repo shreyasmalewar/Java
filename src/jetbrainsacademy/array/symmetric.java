2887b469a5f70c166058e3406dd4b809623c1547
Shreyas <shreyasmalewar@gmail.com>
2020-08-08 11:48:19 +0530

    symmetric array complete

--- src/jetbrainsacademy/array/symmetric.java
+++ src/jetbrainsacademy/array/symmetric.java
@@ -7,14 +7,25 @@ public class symmetric {
         Scanner scanner = new Scanner(System.in);
 
         int n = scanner.nextInt();
-        int[][] matrix = new int[n][n];
+        int[][] arr = new int[n][n];
 
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j< n; j++) {
-                matrix[i][j] = scanner.nextInt();
+            for (int j = 0; j < n; j++) {
+                arr[i][j] = scanner.nextInt();
+            }
+        }
+        scanner.close();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                if (arr[i][j] != arr[j][i]) {
+                    System.out.println("NO");
+                    return;
+                }
             }
         }
 
-        scanner.close();
+        System.out.println("YES");
+
     }
 }
