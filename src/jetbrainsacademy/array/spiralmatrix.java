ba733cc25d3cda87d94e209e41acb45e57d32351
Shreyas <shreyasmalewar@gmail.com>
2020-08-09 23:03:59 +0530

    done with spiral

--- src/jetbrainsacademy/array/spiralmatrix.java
+++ src/jetbrainsacademy/array/spiralmatrix.java
@@ -3,16 +3,24 @@ package jetbrainsacademy.array;
 import java.util.Scanner;
 
 public class spiralmatrix {
+    final static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        initializeMatrix();
+    }
+
+    static void initializeMatrix() {
         int n = scanner.nextInt();
         int[][] matrix = new int[n][n];
+        computeSpiral(matrix, n);
+    }
+
+    static void computeSpiral(int matrix[][], int n) {
         int value = 1;
         int minCol = 0;
         int minRow = 0;
         int maxRow = n - 1;
-        int maxCol = n -1;
-
+        int maxCol = n - 1;
         while (value <= n * n) {
 
             for (int i = minCol; i <= maxCol; i++) {
@@ -40,7 +48,10 @@ public class spiralmatrix {
             maxRow--;
             maxCol--;
         }
+        displaySpiral(matrix);
+    }
 
+    static void displaySpiral(int[][] matrix) {
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix.length; j++) {
                 System.out.printf(matrix[i][j] + " ");
