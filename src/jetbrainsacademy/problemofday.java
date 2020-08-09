4c32f454cf34c047ca1c4a4fce3ea66c5649fc0f
Shreyas <shreyasmalewar@gmail.com>
2020-08-09 19:22:34 +0530

    :sparkles: problem of the day

---
+++ src/jetbrainsacademy/problemofday.java
@@ -0,0 +1,27 @@
+package jetbrainsacademy;
+
+import java.util.Scanner;
+
+public class problemofday {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int numOfElements = scanner.nextInt();
+        int[] array = new int[numOfElements];
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = scanner.nextInt();
+        }
+
+        int element = scanner.nextInt();
+        int count = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == element) {
+                count = count + 1;
+            }
+        }
+
+        System.out.println(count);
+
+        scanner.close();
+    }
+}
