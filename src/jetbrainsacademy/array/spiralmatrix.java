a314878b9d128c9c134846aeeff53c2d915c3772
Shreyas <shreyasmalewar@gmail.com>
2020-08-09 19:33:35 +0530

    :construction: spiral matrix

---
+++ src/jetbrainsacademy/array/spiralmatrix.java
@@ -0,0 +1,12 @@
+package jetbrainsacademy.array;
+
+import java.util.Scanner;
+
+public class spiralmatrix {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+        scanner.close();
+    }
+}
