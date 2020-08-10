ac399101503d8991e81379641dcab30a4e8aed69
Shreyas <shreyasmalewar@gmail.com>
2020-08-10 22:45:50 +0530

    :construction: test case 5 and 6

--- src/jetbrainsacademy/tictactoe.java
+++ src/jetbrainsacademy/tictactoe.java
@@ -18,6 +18,97 @@ public class tictactoe {
         System.out.println("---------");
 
 
+        int xs = 0;
+        int os = 0;
+        boolean xwins = false;
+        boolean owins = false;
 
+        for (int i = 0; i < xo.length; i++) {
+            if (xo[i] == 'O') {
+                os = os + 1;
+            } else if (xo[i] == 'X') {
+                xs = xs + 1;
+            }
+        }
+
+
+
+
+        // check winner horizontally
+        for (int i = 0; i < xo.length; i+= 3) {
+            if (xo[i] + xo[i+1] + xo[i+2] == 264) {
+                xwins = true;
+                break;
+            } else if(xo[i] + xo[i+1] + xo[i+2] == 237) {
+                owins = true;
+                break;
+            }
+        }
+
+        // check winner vertically
+        for (int i = 0; i < 3; i++) {
+            if (xo[i] + xo[i+3] + xo[i+6] == 264) {
+                xwins = true;
+                break;
+            } else if (xo[i] + xo[i+3] + xo[i+6] == 237) {
+                owins = true;
+                break;
+            }
+        }
+/*
+        if (xwins == owins || Math.abs(os - xs) > 1) {
+            System.out.println("Impossible");
+        } else if (Math.abs(os - xs) == 1) {
+            System.out.println("Game not finished");
+        } else if (owins == true) {
+            System.out.println("O wins");
+        } else if (xwins == true) {
+            System.out.println("X wins");
+        }
+*/
+
+        if (owins == true) {
+            System.out.println("O wins");
+        } else if (xwins == true) {
+            System.out.println("X wins");
+        } else if (Math.abs(os - xs) == 1) {
+            System.out.println("Game not finished");
+        } else if (xwins == owins || Math.abs(os - xs) > 1) {
+            System.out.println("Impossible");
+        }
+
+/*
+        if (Math.abs(os - xs) > 1) {
+            System.out.println("Impossible");
+            return;
+        } else if (Math.abs(os - xs) == 1) {
+            System.out.println("Game not finished");
+            return;
+        }
+*/
     }
 }
+
+/*
+        // check winner left diagonal
+        for (int i = 0; i < 1; i++) {
+            if (xo[i] + xo[i + 4] + xo[i + 8] == 264) {
+                System.out.println("X wins");
+                break;
+            } else if (xo[i] + xo[i + 4] + xo[i + 8] == 237) {
+                System.out.println("O wins");
+                break;
+            }
+        }
+
+        // check winner right diagonal
+        for (int i = 2; i < 3; i++) {
+            if (xo[i] + xo[i + 2] + xo[i + 4] == 264) {
+                System.out.println("X wins");
+                break;
+            } else if (xo[i] + xo[i + 2] + xo[i + 4] == 237) {
+                System.out.println("O wins");
+                break;
+            }
+        }
+*/
