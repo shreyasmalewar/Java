62b22e18d814366eed7a9aa5203a574e57c6b656
Shreyas <shreyasmalewar@gmail.com>
2020-08-10 12:12:44 +0530

    :construction: fizzbuzz

---
+++ src/jetbrainsacademy/miscallenous/fizzbuzz.java
@@ -0,0 +1,14 @@
+package jetbrainsacademy.miscallenous;
+
+import java.util.Scanner;
+
+public class fizzbuzz {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        int firstNumber = scanner.nextInt();
+        int lastNumber = scanner.nextInt();
+
+        
+    }
+}
