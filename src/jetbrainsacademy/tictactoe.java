a9da9084ef7e88f8de17efd5b705fc9b2791b09e
Shreyas <shreyasmalewar@gmail.com>
2020-08-16 14:46:27 +0530

    added while loops testing phase

--- src/jetbrainsacademy/tictactoe.java
+++ src/jetbrainsacademy/tictactoe.java
@@ -16,35 +16,64 @@ public class tictactoe {
         }
         System.out.println("---------");
 
-        boolean isEmpty = true;
-        System.out.print(("Enter the coordinates: "));
-        int x = scanner.nextInt();
-        int y = scanner.nextInt();
-        
+        boolean empty = true;
 
-        if (x == 1 && y == 3 && xo[0] == ' ' || xo[0] == '_') {
-            xo[0] = 'X';
-        } else if (x == 2 && y == 3 && xo[1] == ' ' || xo[1] == '_') {
-            xo[1] = 'X';
-        } else if (x == 3 && y == 3 && xo[2] == ' ' || xo[2] == '_') {
-            xo[2] = 'X';
-        } else if (x == 3 && y == 3 && xo[3] == ' ' || xo[3] == '_') {
-            xo[3] = 'X';
-        } else if (x == 1 && y == 2 && xo[4] == ' ' || xo[4] == '_') {
-            xo[4] = 'X';
-        } else if (x == 2 && y == 2 && xo[5] == ' ' || xo[5] == '_') {
-            xo[5] = 'X';
-        } else if (x == 3 && y == 2 && xo[6] == ' ' || xo[6] == '_') {
-            xo[6] = 'X';
-        } else if (x == 1 && y == 1 && xo[7] == ' ' || xo[7] == '_') {
-            xo[7] = 'X';
-        } else if (x == 2 && y == 1 && xo[8] == ' ' || xo[8] == '_') {
-            xo[8] = 'X';
-        } else if (x == 3 && y == 1 && xo[9] == ' ' || xo[9] == '_') {
-            xo[9] = 'X';
-        } else {
-            System.out.println("This cell is occupied! Choose another one!");
+        while (empty == true) {
+
+            int x = 0;
+            int y = 0;
+            boolean perfectCoordinates = false;
+
+            while (perfectCoordinates == false) {
+                System.out.print(("Enter the coordinates: "));
+                x = scanner.nextInt();
+                y = scanner.nextInt();
+ 
+                if (1 <= x && x <= 3 && 1 <= y && y <= 3) {
+                    perfectCoordinates = true;
+                } else if (Character.isLetter(x) || Character.isLetter(y)) {
+                    System.out.println("You should enter numbers!");
+                    perfectCoordinates = false;
+                } else {
+                    System.out.println("Coordinates should be from 1 to 3!");
+                    perfectCoordinates = false;
+                } 
+            }
+
+    
+            if (x == 1 && y == 3 && xo[0] == ' ' || xo[0] == '_') {
+                xo[0] = 'X';
+                empty = false;
+            } else if (x == 2 && y == 3 && xo[1] == ' ' || xo[1] == '_') {
+                xo[1] = 'X';
+                empty = false;
+            } else if (x == 3 && y == 3 && xo[2] == ' ' || xo[2] == '_') {
+                xo[2] = 'X';
+                empty = false;
+            } else if (x == 3 && y == 3 && xo[3] == ' ' || xo[3] == '_') {
+                xo[3] = 'X';
+                empty = false;
+            } else if (x == 1 && y == 2 && xo[4] == ' ' || xo[4] == '_') {
+                xo[4] = 'X';
+                empty = false;
+            } else if (x == 2 && y == 2 && xo[5] == ' ' || xo[5] == '_') {
+                xo[5] = 'X';
+                empty = false;
+            } else if (x == 3 && y == 2 && xo[6] == ' ' || xo[6] == '_') {
+                xo[6] = 'X';
+                empty = false;
+            } else if (x == 1 && y == 1 && xo[7] == ' ' || xo[7] == '_') {
+                xo[7] = 'X';
+                empty = false;
+            } else if (x == 2 && y == 1 && xo[8] == ' ' || xo[8] == '_') {
+                xo[8] = 'X';
+                empty = false;
+            } else {
+                System.out.println("This cell is occupied! Choose another one!");
+                empty = true;
+            }
         }
+
 
         System.out.println("---------");
         for (int i = 0; i < 9; i += 3) {
