f4ac925f570e1db3a268255f5c4f5852800df835
Shreyas <shreyasmalewar@gmail.com>
2020-08-19 17:21:39 +0530

    stage 02 implemented

--- src/jetbrainsacademy/CoffeeMachine.java
+++ src/jetbrainsacademy/CoffeeMachine.java
@@ -1,13 +1,27 @@
 package jetbrainsacademy;
 
+import java.util.Scanner;
+
 public class CoffeeMachine {
     public static void main(String[] args) {
-        System.out.println("Starting to make a coffee");
-        System.out.println("Grinding coffee beans");
-        System.out.println("Boiling water");
-        System.out.println("Mixing boiled water with crushed coffee beans");
-        System.out.println("Pouring coffee into the cup");
-        System.out.println("Pouring some milk into the cup");
-        System.out.println("Coffee is ready!");
+
+        Scanner scanner = new Scanner(System.in);
+
+        int numberOfCups = 0;
+        int water = 200;
+        int milk = 50;
+        int coffeeBeans = 15;
+
+        System.out.print("How many cups of coffee do you want to make: ");
+        numberOfCups = scanner.nextInt();
+        scanner.close();
+
+        System.out.printf("For %d cups of coffee you will need:", numberOfCups);
+        System.out.println();
+        System.out.printf("%d ml of water", numberOfCups * water);
+        System.out.println();
+        System.out.printf("%d ml of milk", numberOfCups * milk);
+        System.out.println();
+        System.out.printf("%d g of coffee beans", numberOfCups * coffeeBeans);
     }
 }
