c1b5b3904019d69be19316e88d58f827a328e986
Shreyas <shreyasmalewar@gmail.com>
2020-08-21 18:58:12 +0530

    :sparkles: BFS

---
+++ src/DAA/BS.java
@@ -0,0 +1,63 @@
+package DAA;
+
+// BFS algorithm in Java
+
+import java.util.*;
+
+public class BS {
+  private int V;
+  private LinkedList<Integer> adj[];
+
+  // Create a BS
+  BS(int v) {
+    V = v;
+    adj = new LinkedList[v];
+    for (int i = 0; i < v; ++i)
+      adj[i] = new LinkedList();
+  }
+
+  // Add edges to the BS
+  void addEdge(int v, int w) {
+    adj[v].add(w);
+  }
+
+  // BFS algorithm
+  void BFS(int s) {
+
+    boolean visited[] = new boolean[V];
+
+    LinkedList<Integer> queue = new LinkedList();
+
+    visited[s] = true;
+    queue.add(s);
+
+    while (queue.size() != 0) {
+      s = queue.poll();
+      System.out.print(s + " ");
+
+      Iterator<Integer> i = adj[s].listIterator();
+      while (i.hasNext()) {
+        int n = i.next();
+        if (!visited[n]) {
+          visited[n] = true;
+          queue.add(n);
+        }
+      }
+    }
+  }
+
+  public static void main(String args[]) {
+    BS g = new BS(4);
+
+    g.addEdge(0, 1);
+    g.addEdge(0, 2);
+    g.addEdge(1, 2);
+    g.addEdge(2, 0);
+    g.addEdge(2, 3);
+    g.addEdge(3, 3);
+
+    System.out.println("Following is Breadth First Traversal " + "(starting from vertex 2)");
+
+    g.BFS(2);
+  }
+}
