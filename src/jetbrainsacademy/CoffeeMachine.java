8a0fbfd2fe71fe36353cce74a4ce9d78d4e374fa
Shreyas <shreyasmalewar@gmail.com>
2020-09-13 13:10:57 +0530

    initialised fixed qty

--- src/jetbrainsacademy/CoffeeMachine.java
+++ src/jetbrainsacademy/CoffeeMachine.java
@@ -2,6 +2,8 @@ package jetbrainsacademy;
 
 class CoffeeMachine {
     public static void main(String[] args) {
-        
+        int fixedMilk = 200;
+        int fixedWater = 50;
+        int fixedCoffee = 15;
     }
 }
