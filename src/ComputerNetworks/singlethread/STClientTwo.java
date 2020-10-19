f299d63c9b5d29abca1de9990402fd2c09577497
Shreyas <shreyasmalewar@gmail.com>
2020-10-19 18:15:50 +0530

    ST Client and server

---
+++ src/ComputerNetworks/singlethread/STClientTwo.java
@@ -0,0 +1,33 @@
+package ComputerNetworks.singlethread;
+
+import java.io.*;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class STClientTwo {
+    private static final String SERVER_ADDRESS = "127.0.0.1";
+    private static final int SERVER_PORT = 34522;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        try (
+            Socket socket = new Socket(SERVER_ADDRESS, SERVER_PORT);
+            DataInputStream input = new DataInputStream(socket.getInputStream());
+            DataOutputStream output  = new DataOutputStream(socket.getOutputStream())
+        ) {
+                
+                String msg = scanner.nextLine();
+            
+                output.writeUTF(msg);
+                String receivedMsg = input.readUTF();
+            
+                System.out.println(receivedMsg);
+            
+            
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    scanner.close();
+    }
+    
+}
---
+++ src/ComputerNetworks/singlethread/STServer.java
@@ -0,0 +1,29 @@
+package ComputerNetworks.singlethread;
+
+import java.io.*;
+import java.net.*;
+
+public class STServer {
+    private static final int PORT = 34522;
+
+    public static void main(String[] args) {
+        try (ServerSocket server = new ServerSocket(PORT)) {
+            while (true) {
+                try (
+                    Socket socket = server.accept(); // accepting a new client
+                    DataInputStream input = new DataInputStream(socket.getInputStream());
+                    DataOutputStream output = new DataOutputStream(socket.getOutputStream())
+                ) {
+
+                        String msg = input.readUTF(); // reading the next client message
+                        output.writeUTF(msg); // resend it to the client
+                        System.out.println("message echoed");
+                    
+                    
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
