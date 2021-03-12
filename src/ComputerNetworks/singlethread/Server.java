ee4003f9f9e4ec0f14e2cf5eddda24fdfbade955
Shreyas <shreyasmalewar@gmail.com>
2021-03-12 13:42:44 +0530

    new client server added

---
+++ src/ComputerNetworks/singlethread/Server.java
@@ -0,0 +1,33 @@
+package ComputerNetworks.singlethread;
+
+import java.io.*;
+import java.net.*;
+
+public class Server {
+    private static final int PORT = 34522;
+
+    public static void main(String[] args) {
+        // System.out.println("Server running");
+
+        try (ServerSocket server = new ServerSocket(PORT)) {
+            while (true) {
+                try (
+                    Socket socket = server.accept(); // accepting a new client
+                    DataInputStream input = new DataInputStream(socket.getInputStream());
+                    DataOutputStream output = new DataOutputStream(socket.getOutputStream())
+                ) {
+
+                        for (int i = 0; i < 5; i++) {
+
+                        String msg = input.readUTF(); // reading the next client message
+                        output.writeUTF(msg); // resend it to the client
+                        System.out.println("message echoed");
+                        
+                        } 
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
